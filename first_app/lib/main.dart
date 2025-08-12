91229518ec48591be4faa272f7fd76bbe195d199
Danielfts <d.trivino@uniandes.edu.co>
2025-08-11 20:10:34 -0500

    completed 39

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,25 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text('hola mundo'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
