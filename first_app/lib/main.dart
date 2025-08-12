2243acaa8fba0dc9ad41bd55e90a643ad22aab51
Danielfts <d.trivino@uniandes.edu.co>
2025-08-11 19:22:56 -0500

    Complete 33

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,10 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('hola mundo'),
+        backgroundColor: Colors.blueGrey,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 98),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Text('hola mundo'),
+          ),
         ),
       ),
     ),
