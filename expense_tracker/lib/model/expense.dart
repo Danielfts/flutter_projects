338d563ebfe343e156bd90939207b6684915c133
Danielfts <d.trivino@uniandes.edu.co>
2025-08-21 16:49:14 -0500

    Feature: completed 101

--- expense_tracker/lib/model/expense.dart
+++ expense_tracker/lib/model/expense.dart
@@ -2,15 +2,19 @@ import 'package:uuid/uuid.dart';
 
 final uuid = Uuid();
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   Expense({
     required this.amount,
     required this.date,
     required this.title,
+    required this.category,
   });
 
   final String id = uuid.v4();
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
