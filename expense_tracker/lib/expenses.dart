8bab22cbbe50b0dab6a03471d95123f536ed0896
Danielfts <d.trivino@uniandes.edu.co>
2025-08-22 12:53:18 -0500

    Feature: completed 107

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -8,13 +10,32 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      amount: 19.99,
+      date: DateTime.now(),
+      title: "Flutter Course",
+      category: Category.work,
+    ),
+    Expense(
+      amount: 19.99,
+      date: DateTime.now(),
+      title: "CSS Course",
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Expense Tracker'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
       body: Column(
         children: [
           Text('The chart'),
-          Text('Expenses list...'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
