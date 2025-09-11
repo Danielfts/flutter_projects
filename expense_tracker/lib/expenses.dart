6505726c7882178c41004a35018d5e253fb196cc
Danielfts <d.trivino@uniandes.edu.co>
2025-09-10 20:59:33 -0500

    Feature: completed 123

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -32,6 +32,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       isScrollControlled: true,
@@ -54,7 +60,12 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onDismissed: removeExpense,
+            ),
+          ),
         ],
       ),
     );
