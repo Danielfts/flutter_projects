a8dbfb244b10b4ef9f3d1ba8ce9d30fc7d059454
Danielfts <d.trivino@uniandes.edu.co>
2025-10-04 16:36:18 -0500

    completed 164

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.meals, required this.title});
@@ -34,17 +35,9 @@ class MealsScreen extends StatelessWidget {
     );
     var mealList = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (ctx, index) {
-        return Card(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Text(
-              meals[index].title,
-              style: whiteText,
-            ),
-          ),
-        );
-      },
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
+      ),
     );
 
     return Scaffold(
